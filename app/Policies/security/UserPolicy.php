7f27c11db9493b8174798a5e28f64deca19e6bf7
viklop <viklop7025@gmail.com>
2025-06-04 11:26:59 -0500

    feat; Crear userPolicy.

--- app/Policies/security/UserPolicy.php
+++ app/Policies/security/UserPolicy.php
@@ -3,18 +3,67 @@
 namespace App\Policies\security;
 
 use App\Models\security\User;
+use App\shared\services\roles\PermissionService;
+use Illuminate\Auth\Access\Response;
 
 class UserPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-
     protected string $menuKey = 'usuarios';
 
-    public function __construct()
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
     {
-        //
+        return false;
+    }
 
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $model): bool
+    {
+        // return false;
+        return PermissionService::userHasPermission($user, $this->menuKey, 'read');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        return false;
     }
 }
